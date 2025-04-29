6fda12aa864e7ee2eaf57a83da392f4e3f8a2270
Ravel Souza <ravelsouza.eletromecanica@gmail.com>
2025-04-29 16:38:17 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import pickle
+import tensorflow as tf
+
+# Carregar scaler e modelo
+with open('scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+
+interpreter = tf.lite.Interpreter(model_path="modelo.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['data']
+    X = np.array(data).reshape(1, -1)
+    X_scaled = scaler.transform(X).astype(np.float32)
+
+    interpreter.set_tensor(input_details[0]['index'], X_scaled)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+
+    return jsonify({'prediction': output_data.tolist()})
+
+if __name__ == '__main__':
+    app.run()
